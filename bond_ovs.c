2c1b79684a2fd941ef3e4dd916590e040db61ce9
sunbo <sunbo.90s@gmail.com>
2017-05-15 04:30:06 -0400

    initial compile

--- bond_ovs.c
+++ bond_ovs.c
@@ -2,7 +2,7 @@
 #include <linux/module.h>
 #include <linux/moduleparam.h>
 
-MODULE_LICENSE("GPL v3");
+MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Sun Bo <sunbo.90s@gmail.com>");
 MODULE_DESCRIPTION("OVS-compatible load balancing bonding");
 
@@ -12,12 +12,11 @@ static int __init bond_ovs_init(void)
 	return 0;
 }
 
-static int __exit bond_ovs_exit(void)
+static void __exit bond_ovs_exit(void)
 {
 	printk(KERN_INFO "bond ovs exit\n");
-	return 0;
 }
 
-module_init(reverse_init);
-module_exit(reverse_exit);
+module_init(bond_ovs_init);
+module_exit(bond_ovs_exit);
 
